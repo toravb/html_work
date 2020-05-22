39d15865112da295fbb1daa8443591433760804f
Viktor <46136749+toravb@users.noreply.github.com>
2020-05-22 17:30:20 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,4 @@
 $(function () {
-//фиксированная шапка
-    let menu = $("#menu");
-    let about = $("#about")
-    let aboutH = about.innerHeight();
-    let scrollPos = $(window).scrollTop();
-
-    checkScroll(scrollPos, aboutH)
-
-    $(window).on("scroll", function () {
-        aboutH = about.innerHeight();
-        scrollPos = $(this).scrollTop();
-
-        checkScroll(scrollPos, aboutH);
-    });
-
-    function checkScroll(scrollPos, aboutH) {
-        if (scrollPos > aboutH) {
-            menu.addClass("fixed");
-        } else {
-            menu.removeClass("fixed")
-        }
-    }
     //плавный скролл
     $("[data-scroll]").on("click", function (event) {
         event.preventDefault();
@@ -34,37 +12,37 @@ $(function () {
     })
 
     //скролл
-    let lastScrollTop = 0;
-    $(window).scroll(function(event){
-        let st = $(this).scrollTop();
-        if (st > lastScrollTop){
-            if (st < 200) {
-                window.scrollTo({
-                    top: 200,
-                    behavior: "smooth"
-                });
-            }
-            if (st > 200) {
-                window.scrollTo({
-                    top: 1200,
-                    behavior: "smooth"
-                });
-            }
-        }else {
-            if (st < 1200 && st > 200) {
-                window.scrollTo({
-                    top: 200,
-                    behavior: "smooth"
-                });
-            }
-            if (st < 200) {
-                window.scrollTo({
-                    top: 0,
-                    behavior: "smooth"
-                });
-            }
-        }
-        lastScrollTop = st;
-    });
+    // let lastScrollTop = 0;
+    // $(window).scroll(function(event){
+    //     let st = $(this).scrollTop();
+    //     if (st > lastScrollTop){
+    //         if (st < 200) {
+    //             window.scrollTo({
+    //                 top: 200,
+    //                 behavior: "smooth"
+    //             });
+    //         }
+    //         if (st > 200) {
+    //             window.scrollTo({
+    //                 top: 1250,
+    //                 behavior: "smooth"
+    //             });
+    //         }
+    //     }else {
+    //         if (st < 1200 && st > 200) {
+    //             window.scrollTo({
+    //                 top: 200,
+    //                 behavior: "smooth"
+    //             });
+    //         }
+    //         if (st < 200) {
+    //             window.scrollTo({
+    //                 top: 0,
+    //                 behavior: "smooth"
+    //             });
+    //         }
+    //     }
+    //     lastScrollTop = st;
+    // });
 });
 
