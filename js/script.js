ba0ca3a607512fa04c65704d19a725546d60972e
Viktor <46136749+toravb@users.noreply.github.com>
2020-05-22 03:48:46 +0300

    update

--- js/script.js
+++ js/script.js
@@ -21,4 +21,50 @@ $(function () {
             menu.removeClass("fixed")
         }
     }
-})
+    //плавный скролл
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let blockId = $(this).data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500)
+    })
+
+    //скролл
+    let lastScrollTop = 0;
+    $(window).scroll(function(event){
+        let st = $(this).scrollTop();
+        if (st > lastScrollTop){
+            if (st < 200) {
+                window.scrollTo({
+                    top: 200,
+                    behavior: "smooth"
+                });
+            }
+            if (st > 200) {
+                window.scrollTo({
+                    top: 1200,
+                    behavior: "smooth"
+                });
+            }
+        }else {
+            if (st < 1200 && st > 200) {
+                window.scrollTo({
+                    top: 200,
+                    behavior: "smooth"
+                });
+            }
+            if (st < 200) {
+                window.scrollTo({
+                    top: 0,
+                    behavior: "smooth"
+                });
+            }
+        }
+        lastScrollTop = st;
+    });
+});
+
